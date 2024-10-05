a829503a46277d25083af9d8097d16d5b3ee24a4
Dante <dante.rivadi@gmail.com>
2024-10-05 08:41:32 -0400

    Basic base template

--- frontend-app/src/app/page.tsx
+++ frontend-app/src/app/page.tsx
@@ -1,9 +1,14 @@
-
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Home() {
   return (
     <>
-      Hola
+      <div className="flex flex-col items-center p-5 gap-5">
+        <h1 className="">Mars Seismic Detection</h1>
+        <a className="text-icon" target="_blank" href={"https://www.spaceappschallenge.org/nasa-space-apps-2024/find-a-team/yvyryryi/"}>
+          Project by Yvyryrýi <BsArrowRight/>
+        </a>
+      </div>
     </>
   );
 }
