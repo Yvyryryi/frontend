02a0b9f6ba4a7f4e03b144c5a5b80c766dec45e4
Dante <dante.rivadi@gmail.com>
2024-10-05 08:14:31 -0400

    Initial config

--- frontend-app/tailwind.config.ts
+++ frontend-app/tailwind.config.ts
@@ -7,10 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "20px",
+          lg: "80px",
+        },
       },
     },
   },
